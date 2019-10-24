5b1ba9cc86913485ae27d7cbfd2b157844b0da71
Arbër <Arber.Rrapaj@hotmail.de>
2019-10-24 11:38:25 +0200

    added port to prod urls

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  countriesUrl: 'https://carrot.rrapaj.com/api/countries',
-  gamesUrl: 'https://carrot.rrapaj.com/api/games',
-  genresUrl: 'https://carrot.rrapaj.com/api/genres',
-  librariesUrl: 'https://carrot.rrapaj.com/api/libraries',
-  usersUrl: 'https://carrot.rrapaj.com/api/users',
-  loginUrl: 'https://carrot.rrapaj.com/api/users/login'
+  countriesUrl: 'https://carrot.rrapaj.com:8123/api/countries',
+  gamesUrl: 'https://carrot.rrapaj.com:8123/api/games',
+  genresUrl: 'https://carrot.rrapaj.com:8123/api/genres',
+  librariesUrl: 'https://carrot.rrapaj.com:8123/api/libraries',
+  usersUrl: 'https://carrot.rrapaj.com:8123/api/users',
+  loginUrl: 'https://carrot.rrapaj.com:8123/api/users/login'
 };
